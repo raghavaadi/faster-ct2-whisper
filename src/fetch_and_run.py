1b3e817bc17aadc8877c28e7d541e4a36d9bfe64
raghavaadi <aadithyaraghav@gmail.com>
2024-10-11 18:18:36 +0530

    Update fetch_and_run.py

--- src/fetch_and_run.py
+++ src/fetch_and_run.py
@@ -3,7 +3,7 @@ from faster_whisper import WhisperModel
 
 # Function to download the model and use FP16 and CUDA
 def download_models():
-    model_names = ["tiny", "base", "large-v2"]  # Update this list if needed
+    model_names = ["tiny", "base", "large-v2","large-v3-turbo-ct2"]  # Update this list if needed
     models = {}
     for model_name in model_names:
         print(f"Downloading model: {model_name}")
